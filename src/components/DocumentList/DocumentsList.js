50431c296bddd9096f80648014a7759c3eac9af8
Gavin McIntosh <gavinmcintosh@gavins-imac-8.home>
2020-05-23 22:37:29 +0100

    changes made to structure and added media queries

--- src/components/DocumentList/DocumentsList.js
+++ src/components/DocumentList/DocumentsList.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
-import DocSearch from '../../components/DocSearch/DocSearch'
 import DocCard from '../../components/DocCard/DocCard'
 
 const DocumentsList = () => {
     const documentList = useSelector(state => state.documentListState.docs)
     return (
-        <div>
-            <DocSearch />
-            <div>
-            {documentList.sort((doc1, doc2) => doc1.lastUpdated.seconds < doc2.lastUpdated.seconds ? 1 : -1).map(doc => <DocCard doc={doc} />)}
-            </div>
+        <div className="doc-list-map">
+            {documentList.sort((doc1, doc2) => 
+            doc1.lastUpdated.seconds < doc2.lastUpdated.seconds ? 1 : -1)
+            .map(doc => <DocCard doc={doc} />)}
         </div>
     )
 }
--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -7,50 +7,49 @@ import DocumentsList from '../components/DocumentList/DocumentsList'
 import Header from '../components/Header/Header'
 import rocketDivider from './rocketDivider.svg'
 import Adverbs from "../components/Notifications/Adverbs"
+import DocSearch from '../components/DocSearch/DocSearch'
 
 export default function Layout() {
     const classes = LayoutStyles();
     return (
       
-      <div>
-      <Header />
-      <section id="main" className="mt-40">
-      <div className="rocket-divider"><img src={rocketDivider} alt="divider" /></div>
-
-          <div className="doc-sidebar p-20">
+      <div id="main-wrapper">
+        <Header />
+        <div className="doc-sidebar">
+            <DocSearch />
             <DocumentsList />
           </div>
 
-          <div className="text-editor-container p-20">
+      <section id="main">
+      <div className="rocket-divider"><img src={rocketDivider} alt="divider" /></div>
+
+          <div className="text-editor-container">
+            <div className="readibility-sidebar">
+              <ReadibilityBox
+                header="Readibility Score"
+                score="6"
+                readingTime="1:35"
+                characters="1056"
+                words="355"
+                sentences="54"
+                paragraphs="5"
+              />
+              <Adverbs />
+              <SEOBox
+                header="SEO Score"
+                score="Good"
+                kwBeginning="Yes"
+                kwContent="Yes"
+                words="450"
+                kwHeadings="No"
+                kwDensity="1.4%"
+                obLinks="2"
+                ibLinks="6"
+              />
+            </div>
             <RichTextWriter />
+
           </div>
-
-          <div className="readibility-sidebar p-20">
-            <ReadibilityBox
-              header="Readibility Score"
-              score="6"
-              readingTime="1:35"
-              characters="1056"
-              words="355"
-              sentences="54"
-              paragraphs="5"
-            />
-            <Adverbs />
-            <SEOBox
-              header="SEO Score"
-              score="Good"
-              kwBeginning="Yes"
-              kwContent="Yes"
-              words="450"
-              kwHeadings="No"
-              kwDensity="1.4%"
-              obLinks="2"
-              ibLinks="6"
-            />
-          </div>
-
-
-          
 
     </section>
     </div>
